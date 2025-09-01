7eb0782dbe292ad12d6d6060af137ac535a7eb0b
snej55 <jenskromdijk@gmail.com>
2025-09-01 15:14:37 +0100

    Add thread pool

--- get_pins_async.py
+++ get_pins_async.py
@@ -2,8 +2,12 @@ import asyncio
 import time
 import datetime
 import aiohttp
+import concurrent.futures
 
 STEP_SIZE = 500
+NUM_THREADS = 8
+THREADS_SIZE = 10000
+THREAD_PIN_INFO = False
 
 async def fetch(session, url):
     async with session.get(url) as response:
@@ -15,11 +19,12 @@ async def scan_pin(pin):
             t = int(time.time()) * 1000
             url = f"https://kahoot.it/reserve/session/{pin}/"
             data = await fetch(session, url)
-            return (pin, data["startTime"])
+            if t - 1000000 < data["startTime"]:
+                return (pin, data["startTime"])
     except Exception as e:
         pass
 
-async def scan(start_pin, end_pin):
+async def scan_range(start_pin, end_pin):
     print(f"--------------- {start_pin} - {end_pin} ---------------")
     # create coroutines
     coroutines = [scan_pin(pin) for pin in range(start_pin, end_pin)]
@@ -29,24 +34,45 @@ async def scan(start_pin, end_pin):
     pins = [result for result in results if result != None]
     pins.sort(key=lambda x: -x[1])
 
-    # for result in pins:
-    #     if result != None:
-    #         print(f"Found pin: {result[0]}, started at {str(datetime.timedelta(seconds=int(result[1] / 1000))).split(' ')[-1]}, {datetime.timedelta(seconds=int(time.time() - result[1] / 1000))} ago")
+    if THREAD_PIN_INFO:
+        print(f"Found {len(pins)} pins")
+        if len(pins):
+            print(f"Best pin: {pins[0][0]}, started at {str(datetime.timedelta(seconds=int(pins[0][1] / 1000))).split(' ')[-1]}, {datetime.timedelta(seconds=int(time.time() - pins[0][1] / 1000))} ago")
 
     return pins
 
-if __name__ == "__main__":
+def scan(start, end):
     pins = []
     print(f'Started scanning at {datetime.timedelta(seconds=time.time())}')
-    
-    for start_pin in range(0, 10000, STEP_SIZE):
-        pins.extend(asyncio.run(scan(start_pin, start_pin + STEP_SIZE)))
+
+    for start_pin in range(start, end, STEP_SIZE):
+        pins.extend(asyncio.run(scan_range(start_pin, start_pin + STEP_SIZE)))
     pins.sort(key=lambda x: -x[1])
     
     print("---------------------------------")
     print("FINISHED SCANNING!")
     print("---------------------------------")
 
-    print("PINS:")
-    for i, pin in enumerate(pins):
-        print(f"{i}. Pin: {pin[0]}, started at {str(datetime.timedelta(seconds=int(pin[1] / 1000))).split(' ')[-1]}, {datetime.timedelta(seconds=int(time.time() - pin[1] / 1000))} ago")
+    if THREAD_PIN_INFO:
+        print("PINS:")
+        for i, pin in enumerate(pins):
+            print(f"{i}. Pin: {pin[0]}, started at {str(datetime.timedelta(seconds=int(pin[1] / 1000))).split(' ')[-1]}, {datetime.timedelta(seconds=int(time.time() - pin[1] / 1000))} ago")
+
+    return pins
+
+
+pins = []
+with concurrent.futures.ThreadPoolExecutor(max_workers = NUM_THREADS) as e:
+    threads = []
+    for i in range(NUM_THREADS):
+        threads.append(e.submit(scan, i * THREADS_SIZE, i * THREADS_SIZE + THREADS_SIZE))
+    
+    for t in concurrent.futures.as_completed(threads):
+        pins.extend(t.result())
+
+pins.sort(key=lambda x: -x[1])
+print("PINS:")
+for i, pin in enumerate(pins):
+    print(f"{i}. Pin: {pin[0]}, started at {str(datetime.timedelta(seconds=int(pin[1] / 1000))).split(' ')[-1]}, {datetime.timedelta(seconds=int(time.time() - pin[1] / 1000))} ago")
+
+    
