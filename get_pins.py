1b66741e8cfdad16ef2b5082dfefe4096b80167d
snej55 <jenskromdijk@gmail.com>
2025-09-01 13:02:45 +0100

    Improve pin scanning

--- get_pins.py
+++ get_pins.py
@@ -7,33 +7,29 @@ import datetime
 print(f"Started at {datetime.timedelta(seconds=int(time.time()))}")
 
 def scan(start, end):
-    progress = 0
     t = int(time.time()) * 1000
     valid = False
     url = ""
     data = {}
     for pin in range(start, end):
+        valid = False
         t = int(time.time()) * 1000
         try:
             url = f"https://kahoot.it/reserve/session/{pin}/"
             data = requests.get(url).json()
             if t - 1000000 < data["startTime"]:
                 valid = True
-                print(f"Started {datetime.timedelta(seconds=int((t - data["startTime"]) / 1000))} ago (pin: {pin})")
+                print(f"Started {datetime.timedelta(seconds=int((t - data["startTime"]) / 1000))} ago (pin: {pin}), at {datetime.timedelta(seconds=int(data["startTime"] / 1000))}")
             else:
                 valid = False
         except Exception:
-            pass
+            continue
             # print("error")
         if valid:
-            print(f"Pin: {pin}")
-            print(f"{progress * 100 :.1f}%")
-
-        valid = False
-        progress = (pin - start) / (end - start)
+            print(f"Pin: {pin} ({pin - start} / {end - start})")
 
 threads = []
-num_threads = 10
+num_threads = 16
 start = 0
 gran = 1000000 / num_threads
 for _ in range(num_threads):
