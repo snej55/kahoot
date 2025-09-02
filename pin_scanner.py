1c6eecfcde603d0edc3f4891b4deabb240385513
snej55 <jenskromdijk@gmail.com>
2025-09-02 12:34:13 +0100

    Fix pins list

--- pin_scanner.py
+++ pin_scanner.py
@@ -5,8 +5,7 @@ import aiohttp
 import concurrent.futures
 from rich.progress import track
 
-NUM_PINS = 20000
-SEMAPHORE = asyncio.Semaphore(500)
+NUM_PINS = 10000
 
 async def fetch(session, url):
     async with session.get(url) as response:
@@ -15,29 +14,30 @@ async def fetch(session, url):
 async def scan_pin(pin):
     try:
         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=1024)) as session:
-            async with SEMAPHORE:
-                await asyncio.sleep(0)
-                data = await fetch(session, f"https://kahoot.it/reserve/session/{pin}/")
-                if time.time() * 1000 - 600000 < data["startTime"]:
-                    print(pin, datetime.timedelta(seconds=int((time.time() * 1000 - data["startTime"]) / 1000)))
-                    return (pin, data["startTime"])
+            await asyncio.sleep(0)
+            data = await fetch(session, f"https://kahoot.it/reserve/session/{pin}/")
+            if time.time() * 1000 - 600000 < data["startTime"]:
+                print(pin, datetime.timedelta(seconds=int((time.time() * 1000 - data["startTime"]) / 1000)))
+                return (pin, data["startTime"])
     except:
         return None
 
 def scan(pin):
-    asyncio.run(scan_pin(pin))
+    return asyncio.run(scan_pin(pin))
 
 if __name__ == "__main__":
     with concurrent.futures.ThreadPoolExecutor() as e:
-        pins = {e.submit(scan, pin): pin for pin in range(NUM_PINS)}
+        futures = {e.submit(scan, pin): pin for pin in range(NUM_PINS)}
 
-        for future in track(concurrent.futures.as_completed(pins), description=f'{NUM_PINS} pins: scanning...', total=NUM_PINS):
+        pins = []
+        for future in track(concurrent.futures.as_completed(futures), description=f'{NUM_PINS} pins: scanning...', total=NUM_PINS):
             try:
                 data = future.result()
+                if data is not None:
+                    pins.append(data)
             except Exception as e:
                 print(e)
 
-pins = [key.result() for key in pins if key.result() is not None]
 pins.sort(key=lambda x: -x[1])
 print("PINS:")
 for i, pin in enumerate(pins):
