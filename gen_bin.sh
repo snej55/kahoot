0b700454b053779c1e92fbd8c2b977385b93518b
snej55 <jenskromdijk@gmail.com>
2025-09-06 11:39:22 +0100

    Add support for multiple files

--- gen_bin.sh
+++ gen_bin.sh
@@ -1,11 +1,19 @@
-echo "Generating binary for $1"
-
-FILE="$(basename -- $1 .py)"
-
-if [ ! -d "bin/$FILE" ]; then
-    mkdir bin/$FILE
+#!/usr/bin/env bash
+if [ ! -d "bin" ]; then
+    mkdir bin
 fi
 
-echo "bin/$FILE"
-cd bin/$FILE
-pyinstaller "../../$FILE.py" --onefile --exclude-module=pygame-ce --icon "../../icon/${path[0]}.ico"
+for n in $(seq 1 $#); do
+    echo "Generating binary for $1"
+    FILE="$(basename -- $1 .py)"
+
+    if [ ! -d "bin/$FILE" ]; then
+        mkdir bin/$FILE
+    fi
+
+    cd bin/$FILE
+    pyinstaller "../../$FILE.py" --onefile --exclude-module=pygame-ce --icon "../../icon/${path[0]}.ico"
+    cd ../..
+    shift
+done
+
